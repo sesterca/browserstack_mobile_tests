b7c0e7ddff62c01d67c7f7cb934ea33e9e3d768e
Maria Osipova <sesterca.mariaosipova@yandex.ru>
2022-10-02 00:52:36 +0300

    add images

--- src/test/java/helpers/Browserstack.java
+++ src/test/java/helpers/Browserstack.java
@@ -1,7 +1,6 @@
 package helpers;
 
 import configuration.BrowserstackConfig;
-import configuration.EmulationConfig;
 import org.aeonbits.owner.ConfigFactory;
 
 import static io.restassured.RestAssured.given;
